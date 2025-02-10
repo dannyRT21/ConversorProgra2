5a8ea3ad7ac594f9b35b94997010fa7d8ea01ff8
dannyRT21 <USSS025824@ugb.edu.sv>
2025-02-10 10:54:53 -0600

    se crean las pestañas (Tabhost) y conversores

--- app/src/main/java/com/ugb/conversorprogra2/MainActivity.java
+++ app/src/main/java/com/ugb/conversorprogra2/MainActivity.java
@@ -1,6 +1,14 @@
 package com.ugb.conversorprogra2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +17,56 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TabHost tbh;
+    Button btn;
+    TextView tempVal;
+    Spinner spn;
+    conversores objConversores = new conversores();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        tbh = findViewById(R.id.tbhConversor);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabMonedas).setIndicator("MONEDAS", null));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("Tiempo").setContent(R.id.tabTiempo).setIndicator("TIEMPO", null));
+        tbh.addTab(tbh.newTabSpec("Almacenamiento").setContent(R.id.tabAlmacenamiento).setIndicator("ALMACENAMIENTO", null));
+        tbh.addTab(tbh.newTabSpec("Transferenciadedatos").setContent(R.id.tabAlmacenamiento).setIndicator("TRANSFERENCIADEDATOS", null));
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opcion = tbh.getCurrentTab();
+
+                spn = findViewById(R.id.spnDeMonedas);
+                int de = spn.getSelectedItemPosition();
+
+                spn = findViewById(R.id.spnAMonedas);
+                int a = spn.getSelectedItemPosition();
+
+                tempVal = findViewById(R.id.txtCantidad);
+                double cantidad = Double.parseDouble(tempVal.getText().toString());
+
+                tempVal = findViewById(R.id.lblRespuesta);
+                double respuesta = objConversores.convertir(opcion, de, a, cantidad);
+                tempVal.setText("Respuesta: "+ respuesta);
+            }
         });
+    }
+}
+class conversores{
+    double[][] valores= {
+            {1,0.98, 7.73, 25.45, 36.78, 508.87, 8.74},//monedas
+            {},//Longitud
+            {},//tiempo
+            {},//Almacenamiento
+            {},//Transferenciadedatos
+    };
+    public double convertir(int opcion, int de, int a, double cantidad){
+        return valores[opcion][a] / valores[opcion][de] * cantidad;
     }
 }
